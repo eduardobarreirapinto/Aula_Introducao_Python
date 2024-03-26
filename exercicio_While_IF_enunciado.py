ab001c440263a8c30dfc6f08d9ba54a0adbf2389
Eduardo B Pinto <eduardobarreirapinto@gmail.com>
2024-03-26 13:16:04 -0300

    inserir .py

---
+++ exercicio_While_IF_enunciado.py
@@ -0,0 +1,10 @@
+# Exercício 1: Verificação de número par ou ímpar
+
+# Exercício 2: Contagem regressiva 10 a zero
+
+# Exercício 3: Verificação de senha
+
+# Exercício 4: Tabuada de um número
+
+# Exercício 5: Calculadora básica
+
